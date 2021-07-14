1daafa36314a1e4c5e35829f226e29452680fe5f
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-07-14 15:33:52 -0600

    Add a 'has a location' test to openmic_spec.rb

---
+++ spec/openmic_spec.rb
@@ -0,0 +1,20 @@
+require './lib/open_mic'
+require './lib/user'
+require './lib/joke'
+
+RSpec.describe OpenMic do
+
+  it 'exists' do
+    open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+
+    expect(open_mic).to be_a(OpenMic)
+  end
+
+  it 'has a location' do
+    open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+
+    expect(open_mic.location).to eq("Comedy Works")
+  end
+
+
+end
