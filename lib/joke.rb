d91fdea980493bac7b76ef90ee3fb5c7d2688e77
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-07-14 14:12:32 -0600

    Add joke class with initialize method and attr_readers

---
+++ lib/joke.rb
@@ -0,0 +1,11 @@
+class Joke
+
+attr_reader :joke_number, :question, :answer
+
+def initialize(joke_number, question, answer)
+  @joke_number = joke_number
+  @question = question
+  @answer = answer
+
+end
+end
