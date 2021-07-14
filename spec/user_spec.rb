4404b72cd20925f694e1d1cde4de67a0300a3dba
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-07-14 14:40:46 -0600

    Add 'can learn a joke' test in user_spec.rb

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -14,4 +14,21 @@ RSpec.describe User do
     expect(user_1.name).to eq("Sal")
   end
 
+  it 'has jokes' do
+    user_1 = User.new("Sal")
+
+    expect(user_1.jokes).to eq([])
+  end
+
+  it 'can learn a joke' do
+    user_1 = User.new("Sal")
+    joke_1 = Joke.new(22, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    joke_2 = Joke.new(13, "How do you keep a lion from charging?", "Take away its credit cards.")
+
+    user_1.learn(joke_1)
+    user_1.learn(joke_2)
+
+    expect(user_1.jokes).to eq([joke_1, joke_2])
+  end
+
 end
