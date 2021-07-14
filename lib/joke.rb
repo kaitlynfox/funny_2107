5201eb3afa50c6c6ae2d89e6936f7b51f66d8c38
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-07-14 14:25:54 -0600

    Change spacing on joke.rb

--- lib/joke.rb
+++ lib/joke.rb
@@ -2,10 +2,9 @@ class Joke
 
 attr_reader :id, :setup, :punchline
 
-def initialize(id, setup, punchline)
-  @id = id
-  @setup = setup
-  @punchline = punchline
-
-end
+  def initialize(id, setup, punchline)
+    @id = id
+    @setup = setup
+    @punchline = punchline
+  end
 end
