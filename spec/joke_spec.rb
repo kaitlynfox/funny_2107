6f6ee8023c82b946740a2a818bdb7b01328ab371
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-07-14 14:17:10 -0600

    Add 'has an id' and 'has a setup' test to joke_spec.rb

--- spec/joke_spec.rb
+++ spec/joke_spec.rb
@@ -2,11 +2,22 @@ require "./lib/joke"
 
 RSpec.describe Joke do
   it 'exists' do
-  joke = Joke.new(22, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    joke = Joke.new(22, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
 
-  expect(joke).to be_a(Joke)
-end
+    expect(joke).to be_a(Joke)
+  end
 
+  xit 'has an id' do
+    joke = Joke.new(22, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+
+    expect(joke.id).to be_a(22)
+  end
+
+  xit 'has a setup' do
+    joke = Joke.new(22, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+
+    expect(joke.setup).to be_a("Why did the strawberry cross the road?")
+  end
 
 
 end
